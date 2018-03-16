6fdafcf1af5ad2583da36e0945373968e3e17645
myyusuf <myyusuf1911@gmail.com>
2018-03-16 23:23:28 +0700

    Add security to urls

---
+++ api/helpers/AuthUtils.js
@@ -0,0 +1,30 @@
+const jwt = require('jsonwebtoken');
+
+const isAuthorizedAsAdmin = function(req, res, next) {
+  const token = req.headers.token;
+
+  jwt.verify(token, process.env.JWT_ENV, function(err, decoded) {
+    if (decoded && decoded.role === 'ADMIN') {
+      next();
+    } else {
+      res.send('Unauthorized', 403);
+    }
+  });
+};
+
+const isAuthenticated = function(req, res, next) {
+  const token = req.headers.token;
+
+  jwt.verify(token, process.env.JWT_ENV, function(err, decoded) {
+    if (decoded) {
+      next();
+    } else {
+      res.send('Unauthorized', 403);
+    }
+  });
+};
+
+module.exports = {
+  isAuthorizedAsAdmin,
+  isAuthenticated,
+};
--- api/routes/roles.js
+++ api/routes/roles.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const RoleController = require('../controllers/roles.js');
 
+const { isAuthorizedAsAdmin } = require('../helpers/AuthUtils');
+
 const router = express.Router();
 
-router.get('/', RoleController.findAll);
+router.get('/', isAuthorizedAsAdmin, RoleController.findAll);
 router.get('/:roleId', RoleController.findOne);
 router.post('/', RoleController.create);
 router.put('/:roleId', RoleController.update);
